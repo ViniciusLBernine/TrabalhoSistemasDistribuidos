5b0168981c69db044776f6852c2c3ecbd32eed70
ViniciusLBernine <137361209+ViniciusLBernine@users.noreply.github.com>
2026-07-07 23:36:17 -0300

    Correções

--- ecommerce-loja/src/main/java/br/edu/utfpr/td/tsi/loja/config/RabbitMQConfig.java
+++ ecommerce-loja/src/main/java/br/edu/utfpr/td/tsi/loja/config/RabbitMQConfig.java
@@ -1,7 +1,5 @@
-package br.edu.utfpr.td.tsi.logistica.config;
+package br.edu.utfpr.td.tsi.loja.config;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
 	@Bean
 	public MessageConverter jsonMessageConverter() {
 		return new Jackson2JsonMessageConverter();
-	}
-
-	@Bean
-	public Queue entregaQueue() {
-		return QueueBuilder.durable("fila.entrega").build();
 	}
 }
